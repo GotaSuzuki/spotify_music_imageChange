5430deef0064d5fbdd8d4c3164f467be599ffdf7
suzuki.gota <suzuki.gota@opst.co.jp>
2024-08-13 18:33:36 +0900

    Headerを画面上部に固定

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import Images from "./components/Images";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Drawer from "./components/Drawer";
-import SettingsIcon from "@mui/icons-material/Settings";
+
+import Header from "./components/Header";
 
 function App() {
   const [userName, setUserName] = useState("");
@@ -46,63 +47,7 @@ function App() {
         open={open}
         setOpen={setOpen}
       />
-      <header
-        style={{
-          position: "fixed",
-          top: "0",
-          width: "100%",
-          height: "80px",
-          zIndex: 1000,
-          backgroundColor: "pink",
-        }}
-      >
-        <nav>
-          <ul
-            style={{
-              display: "flex",
-              margin: 0,
-              padding: 0,
-              listStyleType: "none",
-              height: "80px",
-              alignItems: "center", // 水平中央揃え
-            }}
-          >
-            <li style={{ listStyle: "none", padding: "30px" }}>
-              <Button onClick={toggleDrawer(true)}>
-                <SettingsIcon></SettingsIcon>
-              </Button>
-            </li>
-            <li style={{ listStyle: "none", padding: "30px" }}>
-              <Link to="/">
-                <Button variant="contained" color="primary">
-                  ホーム
-                </Button>
-              </Link>
-            </li>
-            <li style={{ listStyle: "none", padding: "30px" }}>
-              <Link to="/music">
-                <Button variant="contained" color="success">
-                  音楽へ
-                </Button>
-              </Link>
-            </li>
-            <li style={{ listStyle: "none", padding: "30px" }}>
-              <Link to="/change">
-                <Button variant="contained" color="warning">
-                  画像順番変更
-                </Button>
-              </Link>
-            </li>
-            <li style={{ listStyle: "none", padding: "30px" }}>
-              <Link to="/images">
-                <Button variant="contained" color="info">
-                  画像アップロード
-                </Button>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header toggleDrawer={toggleDrawer} />
       <Routes>
         <Route path="/login" element={<Login setUserName={setUserName} />} />
         <Route path="/SignUp" element={<SignUp setUserName={setUserName} />} />
