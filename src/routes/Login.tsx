a83f1cfaba1dad16c6f1ee705fb3bd7311459ca7
GotaSuzuki <go.baseball.0408@icloud.com>
2024-11-21 13:25:02 +0900

    clean: MUIを適用

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Button, Input, InputLabel, Typography } from "@mui/material";
-import { FormControl } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { useSetRecoilState } from "recoil";
 import { userIdState } from "../atoms/useIdState";
 import React from "react";
@@ -41,40 +40,43 @@ const Login = ({ setUserName }) => {
       <Typography variant="h4" component="h1" gutterBottom>
         ログイン
       </Typography>
-      <form onSubmit={onLogin} style={{ width: "100%", maxWidth: "400px" }}>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-          <FormControl>
-            <InputLabel htmlFor="email">メールアドレス</InputLabel>
-            <Input
-              id="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              aria-describedby="email-helper-text"
-            />
-          </FormControl>
-          <FormControl>
-            <InputLabel htmlFor="password">パスワード</InputLabel>
-            <Input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </FormControl>
-          <FormControl>
-            <InputLabel htmlFor="passwordConf">パスワード（確認）</InputLabel>
-            <Input
-              id="passwordConf"
-              type="password"
-              value={passwordConf}
-              onChange={(e) => setPasswordConf(e.target.value)}
-              required
-            />
-          </FormControl>
-        </Box>
+      <Box
+        component="form"
+        onSubmit={onLogin}
+        sx={{
+          width: "100%",
+          maxWidth: "400px",
+          display: "flex",
+          flexDirection: "column",
+          gap: 2
+        }}>
+        <TextField
+          label="メールアドレス"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          fullWidth
+          variant="outlined"
+        />
+        <TextField
+          label="パスワード"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          fullWidth
+          variant="outlined"
+        />
+        <TextField
+          label="パスワード（確認）"
+          type="password"
+          value={passwordConf}
+          onChange={(e) => setPasswordConf(e.target.value)}
+          required
+          fullWidth
+          variant="outlined"
+        />
         <Box sx={{ mt: 3 }}>
           <Button
             type="submit"
@@ -85,7 +87,7 @@ const Login = ({ setUserName }) => {
             ログイン
           </Button>
         </Box>
-      </form>
+      </Box>
     </Box>
   );
 };
