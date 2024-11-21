f427ecd264636fd63e0f1ec7c463e97ec2e9bcf2
GotaSuzuki <go.baseball.0408@icloud.com>
2024-11-21 12:37:33 +0900

    clean: divなどを使用せず、MUIに変更

--- src/routes/ImagesList.tsx
+++ src/routes/ImagesList.tsx
@@ -10,7 +10,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Button, IconButton } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import { SnackbarProvider, closeSnackbar, enqueueSnackbar } from "notistack";
 import CloseIcon from "@mui/icons-material/Close";
 import { imagesLengthSelector, imagesState } from "../atoms/imagesState";
@@ -68,78 +68,71 @@ const ImagesList = () => {
   );
 
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          height: "100vh", // 画面の高さいっぱいに
-          padding: "20px" // 全体の余白
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        padding: 2 // MUIのspacingを使う
+      }}>
+      <Box sx={{ mt: 2 }}>
+        <SnackbarProvider />
+      </Box>
+      <Box>
+        <Typography variant="h6">
+          保存している画像数：{recoilImagesLength}枚
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          overflow: "hidden",
+          minHeight: 0
         }}>
-        <div style={{ marginTop: "20px" }}>
-          <SnackbarProvider />
-        </div>
-        <div>
-          <h2>保存している画像数：{recoilImagesLength}枚</h2>
-        </div>
-        <div
-          style={{
-            flex: 1, // 残りのスペースを占有
-            overflow: "hidden", // はみ出しを隠す
-            minHeight: 0 // Flexboxでの縮小を許可
+        <TableContainer
+          component={Paper}
+          sx={{
+            maxHeight: "100%",
+            overflow: "auto"
           }}>
-          <TableContainer
-            component={Paper}
-            sx={{
-              maxHeight: "100%",
-              overflow: "auto"
-            }}>
-            <Table
-              sx={{ minWidth: 650 }}
-              size="small"
-              aria-label="images table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>画像名</TableCell>
-                  <TableCell align="center">画像</TableCell>
-                  <TableCell align="right">削除ボタン</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {recoilImages.map(
-                  (image: { name: string; publicUrl: string }) => (
-                    <TableRow
-                      key={image.name}
-                      sx={{
-                        "&:last-child td, &:last-child th": { border: 0 }
-                      }}>
-                      <TableCell component="th" scope="row">
-                        {image.name}
-                      </TableCell>
-                      <TableCell align="center">
-                        <img
-                          src={image.publicUrl}
-                          alt={image.name}
-                          style={{ height: "50px", width: "50px" }}
-                        />
-                      </TableCell>
-                      <TableCell align="right">
-                        <Button
-                          variant="contained"
-                          color="error"
-                          onClick={() => handleDelete(image.name)}>
-                          削除
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  )
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
-      </div>
-    </>
+          <Table sx={{ minWidth: 650 }} size="small" aria-label="images table">
+            <TableHead>
+              <TableRow>
+                <TableCell>画像名</TableCell>
+                <TableCell align="center">画像</TableCell>
+                <TableCell align="right">削除ボタン</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {recoilImages.map(
+                (image: { name: string; publicUrl: string }) => (
+                  <TableRow key={image.name}>
+                    <TableCell component="th" scope="row">
+                      {image.name}
+                    </TableCell>
+                    <TableCell align="center">
+                      <img
+                        src={image.publicUrl}
+                        alt={image.name}
+                        style={{ height: 50, width: 50 }}
+                      />
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button
+                        variant="contained"
+                        color="error"
+                        onClick={() => handleDelete(image.name)}>
+                        削除
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                )
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
   );
 };
 
